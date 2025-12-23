0cf58db4ff1232eaa6bebe2cf70e354642a484c7
Anderson Paiva <anderson.adolfo1998@gmail.com>
2025-12-23 12:40:05 -0300

    test(categoria): testes unitários do cado de uso de criação de categoria

---
+++ backend/java/ms-admin-do-catalogo/application/src/test/java/com/codeflix/admin/catalogo/application/test/category/create/CreateCategoryUseCaseTest.java
@@ -0,0 +1,50 @@
+package com.codeflix.admin.catalogo.application.test.category.create;
+
+import com.codeflix.admin.catalogo.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.AdditionalAnswers;
+import org.mockito.Mockito;
+
+import java.util.Objects;
+
+public class CreateCategoryUseCaseTest {
+    // 1. Teste do caminho feliz
+    // 2. Teste simulando um erro com nome inválido
+    // 3. Teste criando uma categoria inativa
+    // 4. Teste simulando um erro genérico vindo do gateway
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        final var expectedName = "";
+        final var expectedDescription = "";
+        final var expectedIsActive = true;
+
+        final var command = CreateCategoryCommand.with(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
+
+        Mockito.when(categoryGateway.create(Mockito.any()))
+                .thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+        final var useCase = new CreateCategoryUseCase(categoryGateway);
+        final var output = useCase.execute(command);
+
+        Assertions.assertNotNull(output);
+
+        Mockito.verify(categoryGateway, Mockito.times(1))
+                .create(Mockito.argThat(category -> {
+                    return Objects.nonNull(category.getId())
+                            && Objects.equals(category.getName(), expectedName)
+                            && Objects.equals(category.getDescription(), expectedDescription)
+                            && Objects.equals(category.isActive(), expectedIsActive)
+                            && Objects.nonNull(category.getCreatedAt())
+                            && Objects.nonNull(category.getUpdatedAt())
+                            && Objects.isNull(category.getDeletedAt());
+                }));
+    }
+}
