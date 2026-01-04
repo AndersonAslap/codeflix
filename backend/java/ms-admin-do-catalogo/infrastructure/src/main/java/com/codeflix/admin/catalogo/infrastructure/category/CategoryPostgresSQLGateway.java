b183df02854b3f076509b87f5fdd48a41dc66e24
Anderson Paiva <anderson.adolfo1998@gmail.com>
2026-01-04 12:40:52 -0300

    feat(categoria-infrastructure): criando a implementação do gateway de categoria

---
+++ backend/java/ms-admin-do-catalogo/infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/category/CategoryPostgresSQLGateway.java
@@ -0,0 +1,46 @@
+package com.codeflix.admin.catalogo.infrastructure.category;
+
+import com.codeflix.admin.catalogo.domain.category.Category;
+import com.codeflix.admin.catalogo.domain.category.CategoryGateway;
+import com.codeflix.admin.catalogo.domain.category.CategoryId;
+import com.codeflix.admin.catalogo.domain.category.CategorySearchQuery;
+import com.codeflix.admin.catalogo.domain.pagination.Pagination;
+import com.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryPostgresSQLGateway implements CategoryGateway {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryPostgresSQLGateway(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Category create(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category update(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(CategoryId categoryId) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryId categoryId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery query) {
+        return null;
+    }
+}
