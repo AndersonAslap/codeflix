301053923380083972da5af626703f84a437267a
Anderson Paiva <anderson.adolfo1998@gmail.com>
2026-01-14 17:35:15 -0300

    test(categoria-infrastructure): testes integrados do caso de uso de criação de categoria

---
+++ backend/java/ms-admin-do-catalogo/infrastructure/src/test/java/com/codeflix/admin/catalogo/application/category/create/CreateCategoryUseCaseIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.codeflix.admin.catalogo.application.category.create;
+
+import com.codeflix.admin.catalogo.IntegrationTest;
+import com.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class CreateCategoryUseCaseIntegrationTest {
+
+    @Autowired
+    private CreateCategoryUseCase createCategoryUseCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        final var expectedName = "filmes";
+        final var expectedDescription = "description";
+        final var expectedIsActive = true;
+
+        Assertions.assertEquals(0, this.categoryRepository.count());
+
+        final var command = CreateCategoryCommand.with(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        final var output = createCategoryUseCase.execute(command).get();
+
+        Assertions.assertEquals(1, this.categoryRepository.count());
+        Assertions.assertNotNull(output);
+        Assertions.assertNotNull(output.categoryId());
+
+        final var categoryRegistered = this.categoryRepository.findById(output.categoryId().getValue()).get();
+
+        Assertions.assertNotNull(categoryRegistered);
+        Assertions.assertEquals(expectedName, categoryRegistered.getName());
+        Assertions.assertEquals(expectedDescription, categoryRegistered.getDescription());
+        Assertions.assertEquals(expectedIsActive, categoryRegistered.isActive());
+        Assertions.assertNotNull(categoryRegistered.getCreatedAt());
+        Assertions.assertNotNull(categoryRegistered.getUpdatedAt());
+        Assertions.assertNull(categoryRegistered.getDeletedAt());
+    }
+}
